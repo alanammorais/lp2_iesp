c3eacd0ed3127a82323687f149d6016f4dcab5ec
Alana MM <alanamm.prof@gmail.com>
2020-08-31 18:09:05 -0300

    update

---
+++ Exemplos de Aula/aula28_08/src/pacoteExemplo/PrimeiraProjeto.java
@@ -0,0 +1,18 @@
+package pacoteExemplo;
+
+public class PrimeiraProjeto {
+	
+	
+	
+	public static void main(String[] args) {	
+		
+		int variavel1;
+		
+		
+		variavel1 = 10;
+		System.out.println(variavel1);
+		
+	} 
+	
+
+}
